07ea4b3b6311f74c7ed81bf351d46127421b4567
qiuxx <1207655804@qq.com>
2017-12-11 16:07:15 +0800

    dev,test,prod properties

--- qiuxx-ssmdz/qiuxx-ssmdz-service/qiuxx-ssmdz-service-user/src/main/java/com/qiuxx/ssmdz/StartUserProvider.java
+++ qiuxx-ssmdz/qiuxx-ssmdz-service/qiuxx-ssmdz-service-user/src/main/java/com/qiuxx/ssmdz/StartUserProvider.java
@@ -1,6 +1,8 @@
 package com.qiuxx.ssmdz;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -9,11 +11,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @Date: 10:50 2017/12/7
  */
 public class StartUserProvider {
-    //private static Logger logger = LoggerFactory.getLogger(StartUserProvider.class);
+    private static Logger logger = LoggerFactory.getLogger(StartUserProvider.class);
     public static void main(String args[]){
-        //logger.info(">>>>>>>>>>>>>>>>>   userCRUDService正在启动  >>>>>>>>>>>>>>>>>>>>>>");
+        logger.info(">>>>>>>>>>>>>>>>>   userCRUDService正在启动  >>>>>>>>>>>>>>>>>>>>>>");
         new ClassPathXmlApplicationContext(new String[]{"classpath:spring/spring-context.xml"});
-      //  logger.info(">>>>>>>>>>>>>>>>>   userCRUDService启动完成  >>>>>>>>>>>>>>>>>>>>>>");
+        logger.info(">>>>>>>>>>>>>>>>>   userCRUDService启动完成  >>>>>>>>>>>>>>>>>>>>>>");
         while (true){
             try {
                 Thread.sleep(1000);
