141bba641f614483dd26e129ab5d1ff0076c1866
qiuxx <1207655804@qq.com>
2017-12-11 17:12:11 +0800

    druid

--- qiuxx-ssmdz/datamodel/user.sql
+++ qiuxx-ssmdz/datamodel/user.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2017-12-08 23:58:24
+Date: 2017-12-11 17:11:21
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,22 +20,23 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `user_id` bigint(15) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `user_Id` tinyint(15) NOT NULL AUTO_INCREMENT COMMENT '主键Id',
   `user_name` varchar(8) NOT NULL COMMENT '用户姓名',
-  `user_telephone` varchar(18) NOT NULL COMMENT '用户手机号',
-  `user_email` varchar(18) NOT NULL COMMENT '用户邮箱',
-  `user_address` varchar(40) NOT NULL COMMENT '用户所在地',
-  `user_description` varchar(80) NOT NULL COMMENT '用户相关描述',
-  `is_deleted` char(1) NOT NULL DEFAULT 'N' COMMENT '删除标志，N:为删除，Y:已删除',
+  `user_telephone` varchar(14) NOT NULL COMMENT '用户手机号',
+  `user_address` varchar(50) NOT NULL COMMENT '用户地址',
+  `user_email` varchar(20) NOT NULL COMMENT '用户邮箱',
+  `user_description` varchar(100) NOT NULL COMMENT '用户描述信息',
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '删除时间',
-  PRIMARY KEY (`user_id`),
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `is_deleted` char(1) NOT NULL DEFAULT 'N' COMMENT '删除标识，N:未删除，Y:已删除',
+  PRIMARY KEY (`user_Id`),
   UNIQUE KEY `idx_user_name` (`user_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', '王五', '1894232433', '3241234313@qq.com', '北京', '这个人在北京', 'N', '2017-12-08 22:02:18', '2017-12-08 23:41:49');
-INSERT INTO `user` VALUES ('4', '李四', '1894232433', '3241234313@qq.com', '天津', '这个人在天津', 'N', '2017-12-08 23:48:27', '2017-12-08 23:48:27');
-INSERT INTO `user` VALUES ('5', '张三', '1894232433', '3241234313@qq.com', '天津', '这个人在天津', 'N', '2017-12-08 23:57:56', '2017-12-08 23:57:56');
+INSERT INTO `user` VALUES ('1', '张三', '18942306374', '杭州', '1207655804@qq.com', '这是一个在杭州的人', '2017-12-06 14:00:46', '2017-12-06 14:00:46', 'N');
+INSERT INTO `user` VALUES ('2', '李四', '12421423143', '上海', '2343122@qq.com', '这是一个在上海的人', '2017-12-08 17:50:27', '2017-12-11 17:08:10', 'N');
+INSERT INTO `user` VALUES ('3', '王五', '12421423143', '北京', '2343122@qq.com', '这是一个在北京的人', '2017-12-08 17:50:27', '2017-12-11 17:08:22', 'N');
+INSERT INTO `user` VALUES ('4', '赵六', '12421423143', '深圳', '2343122@qq.com', '这是一个在深圳的人', '2017-12-08 17:51:32', '2017-12-11 17:08:36', 'N');
